959db9567adc1773e64f308e7d416176f5e4466f
TomBombadilom <tom@croll.fr>
2024-02-01 22:20:38 +0100

    added trasparency to preloading 	modified:   src/lib/loading.tsx

--- src/lib/loading.tsx
+++ src/lib/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex w-screen h-screen justify-center items-center text-center text-primary-foreground bg-primarey dark:bg-primary dark:text-primary-foreground ">
+    <div className="flex w-screen h-screen justify-center items-center text-center text-primary-foreground bg-primary bg-opacity-10 dark:bg-primary dark:bg-opacity-10 dark:text-primary-foreground ">
       <div className='loading'>
         {/* SVG path to display a loading animation */}
         <svg
