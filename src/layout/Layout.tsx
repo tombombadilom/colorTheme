551f5d6e30d767cb1166c60dce0d732af4df6d87
TomBombadilom <tom@croll.fr>
2024-03-06 03:37:51 +0100

    added some transparency 	modified:   src/layout/Layout.tsx 	modified:   src/templates/page.tsx

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -16,7 +16,7 @@ const Sheet = lazy(() => import('./Sheet'));
 const Layout = ({ children }: { children: ReactNode }): ReactNode => (
   <div className="layout w-[100vw] min-h-[100dvh] bg-background text-primary-foreground dark:bg-background dark:text-primary-foreground h-dvh flex flex-col overflow-x-hidden transition-bg">
     <Sheet>
-      <div className="flex flex-col items-center justify-center w-[100vw] min-h-[90dvh] overflow-y-auto text-align-center bg-transparent">
+      <div className="flex flex-col backdrop-blur-md bg-opacity-light hover:bg-opacity-medium dark:bg-opacity-light dark:hover:bg-opacity-medium items-center justify-center w-[100vw] min-h-[90dvh] overflow-y-auto text-align-center bg-transparent">
         {children}
       </div>
     </Sheet>
--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -4,7 +4,7 @@ import MailPage from './examples/mail/page';
 
 const IndexPage: React.FC = () => {
   return (
-    <div className="w-full h-[90dvh] overflow-hidden rounded-lg border bg-background shadow-lg">
+    <div className="w-full h-[90dvh] backdrop-blur-md bg-opacity-light hover:bg-opacity-medium dark:hover:bg-opacity-medium dark:bg-opacity-light overflow-hidden rounded-lg border bg-background-transparent shadow-lg">
       <MailPage />
     </div>
   );
