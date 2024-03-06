513ca7a9e4ac98bd9120d7e58e20a0bfec7d9db2
TomBombadilom <tom@croll.fr>
2024-03-06 01:08:55 +0100

    deleted old canvas 	modified:   postcss.config.js 	modified:   src/App.css 	modified:   src/App.tsx 	deleted:    src/lib/BackgroundCanvas/BackgroundCanvas.tsx 	deleted:    src/lib/BackgroundCanvas/Canvas.tsx 	deleted:    src/lib/BackgroundCanvas/CanvasBackground.tsx 	deleted:    src/lib/BackgroundCanvas/Plasma.tsx 	deleted:    src/lib/BackgroundCanvas/colorPaletteGenerator.tsx 	deleted:    src/lib/BackgroundCanvas/index.ts 	deleted:    src/lib/BackgroundCanvas/shader/plasma.frag 	deleted:    src/lib/BackgroundCanvas/shader/plasma.vert 	deleted:    src/lib/BackgroundCanvas/shader/shaders.d.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,43 @@
-import { lazy, Suspense } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import './App.css';
-import { ThemeProvider } from './lib/ThemeProvider';
+import { useTheme, ThemeProvider } from './lib/ThemeProvider';
 import loading from './lib/loading';
 import { ErrorBoundary } from './lib/ErrorBoundary';
-// import Canvas from './lib/BackgroundCanvas/Canvas';
+import { Background } from './lib/background/Background';
+
 const Routes = lazy(() => import('./Routes'));
 
 function App() {
+  const { isDarkMode } = useTheme();
+  // Function to enter full screen
+  const enterFullScreen = () => {
+    const elem = document.documentElement;
+
+    if (!document.fullscreenElement) {
+      elem.requestFullscreen().catch((e) => {
+        console.error(`Error attempting to enable full-screen mode: ${e.message} (${e.name})`);
+      });
+    }
+  };
+
+  useEffect(() => {
+    // Automatically enter full screen on component mount
+    enterFullScreen();
+
+    // Clean up
+    return () => {
+      // Exit full screen when component unmounts
+      if(document.fullscreenElement) {
+        document.exitFullscreen();
+      }
+    };
+  }, []);
+  
   return (
     <ErrorBoundary>
       <Suspense fallback={loading()}>
+        <Background speed={0.25} isDarkMode={isDarkMode}/>
         <ThemeProvider defaultTheme={'dark'} storageKey={'vite-ui-theme'}>
-          {/* <Canvas defaultBackground={"none"} /> */}
           <Routes />
         </ThemeProvider>
       </Suspense>
